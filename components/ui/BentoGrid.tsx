dee90025331add452b8b789a9d2969434502f2bd
Mac <mac@Macs-MacBook-Pro.local>
2024-07-08 23:54:01 +0530

    text updates

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -53,8 +53,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["ReactJS", "Express", "Typescript","Docker"];
+  const rightLists = ["ReactJs", "NextJs", "Angular"];
 
   const [copied, setCopied] = useState(false);
 
